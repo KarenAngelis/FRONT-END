b721619de241fea6c4af7fe02ee3174492e25a42
Karen <karen.denich@gmail.com>
2024-05-15 06:34:54 -0300

    atualização

--- vite-project/src/pages/Details/index.jsx
+++ vite-project/src/pages/Details/index.jsx
@@ -1,10 +1,26 @@
-import "./style.css"
+import { Container, Links } from './style'
+
+import { Header } from '../../components/Header'
+import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
 
 export function Details() {
   return (
-    <>
-      <h1>Hello World!</h1>
-      <span>Rodrigo Gonçalves</span>
-    </>
+    <Container>
+      <Header />
+
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">https://rocketseat.com.br</a>
+          </li>
+          <li>
+            <a href="#">https://rocketseat.com.br</a>
+          </li>
+        </Links>
+      </Section>
+
+      <Button title="Voltar" />
+    </Container>
   )
 }
